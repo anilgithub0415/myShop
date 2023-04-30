d530599aa1bd32deefe0c6f61387bfd661bb962f
anilgithub0415 <119879135+anilgithub0415@users.noreply.github.com>
2023-04-30 19:21:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,4 +36,5 @@ app.use(express.static(__dirname + '/myShop'));
 app.get('/*', function(req,res) {
    res.sendFile(path.join(__dirname+'/dist/my-Shop/index.html'));
  });
+ 
 app.listen(port, () => console.log(`Server started on port ${port}`)); */
