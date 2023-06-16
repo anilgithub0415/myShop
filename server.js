0e9029ef999590e13e134e1eafac90928ea183d1
anilgithub0415 <119879135+anilgithub0415@users.noreply.github.com>
2023-06-16 10:16:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the dist directory
-//app.use(express.static(path.join(__dirname, 'dist', 'my-eshop')));
-//app.use(express.static('./dist/my-eshop'));
-app.use(express.static(path.join(__dirname, 'my-eshop')));
+//app.use(express.static(path.join(__dirname, 'dist', 'eshop')));
+//app.use(express.static('./dist/eshop'));
+app.use(express.static(path.join(__dirname, 'eshop')));
 
 app.get('*.js', (req, res, next) => {
     req.url = req.url + '.gz';
@@ -25,7 +25,7 @@ app.get('*.js', (req, res, next) => {
 
 
 //Option 1
-/**/ app.use(express.static(path.join(__dirname, 'dist', 'my-eshop')
+/**/ app.use(express.static(path.join(__dirname, 'dist', 'eshop')
 , {
     setHeaders: (res, path, stat) => {
         if (path.endsWith('.js')) {
@@ -60,7 +60,7 @@ app.get('*.js', (req, res, next) => {
     }); */
 
 
-/* app.use(express.static(path.join(__dirname, 'dist', 'my-eshop')
+/* app.use(express.static(path.join(__dirname, 'dist', 'eshop')
 , {
     setHeaders: (res, path, stat) => {
         if (path.endsWith('.js')) {
@@ -106,24 +106,24 @@ app.use((req, res, next) => {
 app.get('*', (req, res) => {
  // res.sendFile(path.join(__dirname, 'dist', 'my-eshop', 'index.html'));
  //res.sendFile(path.join('/dist', 'my-Shop', 'index.html'));
- //res.sendFile('index.html', { root: __dirname + '/dist/my-eshop' });
+ //res.sendFile('index.html', { root: __dirname + '/dist/eshop' });
 
  res.setHeader('Content-Type', 'text/html');
  //res.sendFile(path.join(__dirname, 'my-eshop', 'index.html'));
- res.sendFile(path.join(__dirname, 'my-eshop/index.html'));
+ res.sendFile(path.join(__dirname, 'eshop/index.html'));
 });
 
 // Start the server on port 8081
 const port = process.env.PORT || 4200;
 app.listen(port, () => {
-    console.log('finding index.html at:'+path.join('dist', 'my-eshop', 'index.html'))
+    console.log('finding index.html at:'+path.join('dist', 'eshop', 'index.html'))
   console.log(`Server started on port ${port} and __dirname:${__dirname}`);
 });
 /* const express = require('express');
 const app = express();
 const path = require('path');
 // Add MIME type configuration for JavaScript files
-app.use(express.static('dist/my-Shop', {
+app.use(express.static('dist/EShop', {
     setHeaders: (res, path, stat) => {
         if (path.endsWith('.js')) {
             res.set('Content-Type', 'application/javascript');
@@ -133,10 +133,10 @@ app.use(express.static('dist/my-Shop', {
 
 // Serve index.html file
 // app.get('*', (req, res) => {
-//    res.sendFile('my-Shop/index.html', { root: '.' });
+//    res.sendFile('EShop/index.html', { root: '.' });
 //}); 
 app.get('/*', function(req,res) {
-   res.sendFile(path.join(__dirname+'dist/my-Shop/index.html'));
+   res.sendFile(path.join(__dirname+'dist/EShop/index.html'));
  });
 
 // Start the server
